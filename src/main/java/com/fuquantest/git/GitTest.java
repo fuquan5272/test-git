b9ea15818af6e243b0a09ae39680b34824366b67
fuquan <18976015272@163.com>
2022-01-06 16:47:15 +0800

    master first commit!

--- src/main/java/com/fuquantest/git/GitTest.java
+++ src/main/java/com/fuquantest/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git, welcome!");
         System.out.println("hello git, 3333!");
         System.out.println("hello git, hot-fix in!");
+        System.out.println("master test!");
     }
 }
