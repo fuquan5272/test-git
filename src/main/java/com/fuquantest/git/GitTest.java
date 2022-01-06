4193242a3647f70bc65cd3dc0c101c6e611a3f88
fuquan <18976015272@163.com>
2022-01-06 15:48:25 +0800

    IDEA second time commit!

--- src/main/java/com/fuquantest/git/GitTest.java
+++ src/main/java/com/fuquantest/git/GitTest.java
@@ -3,5 +3,6 @@ package com.fuquantest.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git, test!");
+        System.out.println("hello git, welcome!");
     }
 }
